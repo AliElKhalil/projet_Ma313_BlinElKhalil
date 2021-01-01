5a46691167ae50d8dcbed3077f9825b458f06645
AliElKhalil <ali.el-khalil@ipsa.fr>
2021-01-01 20:39:39 +0100

    Update Projet_Partie_3.py

--- Projet_Partie_3.py
+++ Projet_Partie_3.py
@@ -30,8 +30,12 @@ def RegressionLineaireCercle(X,Y):
         centre et rayon du cercle de regression lineaire.
 
     """
-    X=[X]
-    Y=[Y]
+    j=np.shape(X)[0]
+    if np.shape(X)==(j,):
+        X=[X]
+    j=np.shape(Y)[0]
+    if np.shape(Y)==(j,):
+        Y=[Y]
     Xt=np.transpose(X)
     Yt=np.transpose(Y)
     n=np.shape(Xt)
@@ -103,9 +107,114 @@ def ComparaisonPointsRegression(X,Y):
     """
     [[a],[b],[g]]=RegressionLineaireCercle(X,Y)
     R=np.sqrt(g+a**2+b**2)
+    alpha=a
+    beta=b
     theta=np.linspace(0,2*np.pi,1000)
     x=R*np.cos(theta)+a
     y=R*np.sin(theta)+b
-    plt.plot(x,y)
-    plt.scatter(X,Y)
+    plt.plot(x,y,label="régression linéaire")
+    plt.scatter(a,b,label="centre du cercle")
+    plt.scatter(X,Y,label="points étudiés")
+    plt.title("Comparaison des points avec la regression linéaire")
+    plt.legend()
+    plt.legend(loc='upper left')
     plt.show()
+    
+
+def ResolutionPartie3():
+    X,Y=donnees_partie3()
+    ComparaisonPointsRegression(X, Y)
+    
+    
+def RegressionLineaireEllipse(X,Y):
+    X=[X]
+    Y=[Y]
+    Xt=np.transpose(X)
+    Yt=np.transpose(Y)
+    n=np.shape(Xt)
+    A=np.concatenate((Xt*Xt,Xt*Yt,Yt*Yt,Xt,Yt,np.ones(n)),axis=1)
+    B=np.ones(n)
+    M=ResolMCNP(A,B)
+    return M
+
+ 
+def TraceEllipse(a,b,alpha,beta,teta):
+    c=max(a,b)
+    gamma=max(abs(alpha),abs(beta))
+    x = -np.linspace(-c-gamma,c+gamma,1000)
+    y = np.linspace(-c-gamma,c+gamma,1000)
+    X,Y = np.meshgrid(x,y)
+    A=(np.cos(teta)**2)/(a**2)+(np.sin(teta)**2)/(b**2)
+    B=2*np.cos(teta)*np.sin(teta)
+    C=(np.cos(teta)**2)/(b**2)+(np.sin(teta)**2)/(a**2)
+    h=A+C
+    D=-2*alpha*A-B*beta*h
+    E=-2*beta*C-B*alpha*h
+    F=(((alpha**2)*(np.cos(teta)**2)+(beta**2)*(np.sin(teta)**2))/(a**2))+(((beta**2)*(np.cos(teta)**2)+(alpha**2)*(np.sin(teta)**2))/(b**2))
+    eqn = A*(X**2)+B*X*Y+C*(Y**2)+D*X+E*Y+F
+    Z = 1
+    plt.xlim([-c-gamma,c+gamma])
+    plt.ylim([-c-gamma*c,c+gamma])
+    plt.contour(X,Y,eqn,[Z])
+    plt.grid()
+    plt.show()
+    
+    
+   
+def tracerEllipseRegression(X,Y):
+    M=RegressionLineaireEllipse(X,Y)
+    A=M[0][0]
+    B=M[1][0]
+    C=M[2][0]
+    D=M[3][0]
+    E=M[4][0]
+    F=M[5][0]
+    teta=np.arcsin(B)/2
+    b=np.sqrt((-(np.tan(teta)**2)*((np.sin(teta)**2)+(np.cos(teta)**2)))/C*(1-(A*np.tan(teta)**2)/C))
+    a=np.sqrt(((b**2)*(np.cos(teta)**2))/((A*(b**2))-(np.sin(teta)**2)))
+    h=A+C
+    beta=(-2*E*A+B*D)/(2*A*(2*C+((h**2)*B**2)/(-2*A)))
+    alpha=(D+B*beta*h)/(-2*A)
+    beta=b*np.sqrt(E)
+    if a-b>0:
+        c=a
+    else:
+        c=b
+    if np.abs(alpha)-np.abs(beta)>0:
+        gamma=np.abs(alpha)
+    else:
+        gamma=np.abs(beta)
+    p = -np.linspace(-c-gamma,c+gamma,1000)
+    q = np.linspace(-c-gamma,c+gamma,1000)
+    x,y = np.meshgrid(p,q)
+    eqn = A*(x**2)+B*y*x+C*(y**2)+D*x+E*y+F
+    Z = 1
+    plt.contour(x,y,eqn,[Z])
+    plt.show()
+    
+def RegressionLineaireAffine(X,Y):
+    j=np.shape(X)[0]
+    if np.shape(X)==(j,):
+        X=[X]
+    j=np.shape(Y)[0]
+    if np.shape(Y)==(j,):
+        Y=[Y]
+    Xt=np.transpose(X)
+    Yt=np.transpose(Y)
+    M=np.concatenate(Xt,np.ones(np.shape(Xt)))
+    return ResolMCQR(M,Yt)
+
+def TracerDroiteRegression(X,Y):
+    [[a],[b]]=RegressionLineaireAffine(X, Y)
+    Mx=max(X)
+    mx=min(X)
+    My=max(Y)
+    my=min(Y)
+    p = -np.linspace(mx-1,Mx+1,1000)
+    q = np.linspace(my-1,My+1,1000)
+    x,y = np.meshgrid(p,q)
+    eqn = a*x+b
+    Z = y
+    plt.contour(x,y,eqn,[Z])
+    plt.show()
+    
